55a7a7fb2086368b4dc2becc54c8420fdba3d44b
pengjie_yao <pengjie_yao@dachuangtech.com>
2019-09-16 16:11:49 +0800

    主分支

--- gitflow-demo1/src/java/gitflowTest.java
+++ gitflow-demo1/src/java/gitflowTest.java
@@ -9,20 +9,8 @@ package java;
 public class gitflowTest {
 
     public static void main(String[] args) {
-        System.out.println("test1");
-        System.out.println("developtest2");
-        featureOne();
+        System.out.println("master");
     }
 
-    public static void featureOne() {
-        System.out.println("这是功能开发1");
-    }
 
-    public static void featureTwo() {
-        System.out.println("功能2");
-    }
-
-    public static void test3() {
-        System.out.println("功能3");
-    }
 }
